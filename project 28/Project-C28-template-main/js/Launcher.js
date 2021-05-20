e8816ce8568c3ebf8be66ef4730b67e6abb07f5f
AdityaSoni08 <81225082+AdityaSoni08@users.noreply.github.com>
2021-05-20 20:24:10 +0530

    Add files via upload

---
+++ project 28/Project-C28-template-main/js/Launcher.js
@@ -0,0 +1,36 @@
+class launcher{
+    constructor(bodyA,pointB){
+        //constructor(bodyA,pointB)
+    var options={
+        bodyA:bodyA,
+        pointB:pointB,
+       // bodyB:bodyB,
+        length:100,
+        stiffness:0.04
+    
+    }
+    this.pointB=pointB;
+    this.launcher=Constraint.create(options);
+    World.add(world,this.launcher);
+    
+    
+    }
+    fly(){
+    this.launcher.bodyA=null;
+    
+    }
+    display(){
+    if(this.launcher.bodyA){
+        var pointA=this.launcher.bodyA.position;
+        var pointB=this.pointB
+        //var pointB=this.sling.bodyB.position;
+        push ();
+        strokeWeight(5);
+        line (pointA.x,pointA.y,pointB.x,pointB.y);
+        pop ();
+    }
+    
+    }
+        
+    }
+    
